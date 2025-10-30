a774ab5ac3aa6379f8b04b1e3ae73e29c45a2a62
kedex <maximumbid30@gmail.com>
2025-10-30 16:37:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const menu = document.querySelector('.sidebar_byrger_menu');
+const menuOverlay = document.querySelector('.menu_overlay');
+const closeMenuOverlay = document.querySelector('.menu_overlay_close');
+const bookingBtn = document.querySelector('.menu_nav_button');
+const bookingList = document.querySelector('.header_list_button');
+const bookingContact = document.querySelector('.contact_map_button')
+const bookingModal = document.querySelector('.booking_mobal');
+const bookingForm = document.querySelector('.booking_content_button');
+const closeBooking = document.querySelector('.menu_booking_close')
+
+menu.addEventListener('click', () => {
+    menuOverlay.classList.add('active');
+});
+
+closeMenuOverlay.addEventListener('click', () => {
+    menuOverlay.classList.remove('active');
+});
+
+bookingBtn.addEventListener('click', () => {
+    menuOverlay.classList.remove('active');
+    bookingModal.classList.add('active');
+});
+
+bookingList.addEventListener('click', () => {
+    bookingModal.classList.add('active');
+});
+
+bookingContact.addEventListener('click', () => {
+    bookingModal.classList.add('active');
+});
+
+closeBooking.addEventListener('click', () => {
+    bookingModal.classList.remove('active');
+});
+
+bookingForm.addEventListener('click', () => {
+    bookingModal.classList.remove('active');
+});
